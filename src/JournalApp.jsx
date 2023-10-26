540d5920504eab9b875cb9fccad7bfa3aa4c214c
Marcelo Puebla <marcenp23@gmail.com>
2023-10-25 23:43:32 -0300

    feat(routes): added routes configuration

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { AppRouter } from './router/AppRouter';
 
 export const JournalApp = () => {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <AppRouter />
     </>
   );
 };
