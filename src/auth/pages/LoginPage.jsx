c9db9a6a2e5f8e96b3aef1ad0697cff6de59357b
Marcelo Puebla <marcenp23@gmail.com>
2023-10-28 16:57:23 -0300

    style(classes): added box shadow css class

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,32 @@
+import { Grid, TextField, Typography } from '@mui/material';
 import React from 'react';
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}>
+      <Grid item className="box-shadow" xs={3} sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}>
+        <Typography variant="h5" sx={{ mb: 1 }}>
+          Login
+        </Typography>
+
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField label="Mail" type="email" placeholder="mail@example.com" fullWidth></TextField>
+            </Grid>
+
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField label="Password" type="password" placeholder="*******" fullWidth></TextField>
+            </Grid>
+          </Grid>
+        </form>
+      </Grid>
+    </Grid>
+  );
 };
