420eb16484150982c4e4e0502b21bfe46e00cd7c
Marcelo Puebla <marcenp23@gmail.com>
2023-11-03 08:55:32 -0300

    feat(login-page): created design for the page

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,5 +1,7 @@
-import { Grid, TextField, Typography } from '@mui/material';
+import { Google } from '@mui/icons-material';
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 
 export const LoginPage = () => {
   return (
@@ -23,6 +25,26 @@ export const LoginPage = () => {
 
             <Grid item xs={12} sx={{ mt: 2 }}>
               <TextField label="Password" type="password" placeholder="*******" fullWidth></TextField>
+            </Grid>
+
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  <Google>Google</Google>
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+
+            <Grid container direction="row" justifyContent="end">
+              <Link component={RouterLink} color="inherit" to="/auth/register">
+                Create an account
+              </Link>
             </Grid>
           </Grid>
         </form>
