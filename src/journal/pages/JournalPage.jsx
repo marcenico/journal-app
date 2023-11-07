7c94ee8a5eaa725d724ef46702f2bdf0b2a268ff
Marcelo Puebla <marcenp23@gmail.com>
2023-11-06 23:02:02 -0300

    feat(note-view): created note view with image gallery

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { JournalLayout } from '../layout/JournalLayout';
-import { NothingSelectedView } from '../views';
+import { NoteView } from '../views';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NothingSelectedView />
+      {/* <NothingSelectedView /> */}
+      <NoteView />
     </JournalLayout>
   );
 };
