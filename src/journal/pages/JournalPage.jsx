47cbd1ca3fc9491562c2550df9792328838b2675
Marcelo Puebla <marcenp23@gmail.com>
2023-11-06 23:07:54 -0300

    feat(journal-page): added fab buttton

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { JournalLayout } from '../layout/JournalLayout';
-import { NoteView } from '../views';
+import { NoteView, NothingSelectedView } from '../views';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView />
+      <NothingSelectedView />
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.8 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50,
+          transition: 'all 0.3s ease'
+        }}>
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
+      {/* <NoteView /> */}
     </JournalLayout>
   );
 };
