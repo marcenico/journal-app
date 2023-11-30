f6c5491f9fd95d8cfdae06471f664a3c166ee748
Marcelo Puebla <marcenp23@gmail.com>
2023-11-29 22:18:15 -0300

    feat(register-page): added onsubmit method

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,25 +1,62 @@
-import { Google } from '@mui/icons-material';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+
+const formData = {
+  email: 'mail@example.com',
+  password: '123456',
+  displayName: 'John'
+};
 
 export const RegisterPage = () => {
+  const { displayName, email, password, onInputChange, formState } = useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <AuthLayout title="Register">
       <form>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="Full name" type="text" placeholder="John" fullWidth></TextField>
+            <TextField
+              label="Full name"
+              type="text"
+              placeholder="John"
+              fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}></TextField>
           </Grid>
 
           <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField label="Password" type="password" placeholder="*******" fullWidth></TextField>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="mail@example.com"
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}></TextField>
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="*******"
+              fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}></TextField>
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+              <Button variant="contained" fullWidth onClick={onSubmit}>
                 Create
               </Button>
             </Grid>
