7ef480fa6a4b2cf7c2f29170bdce5a9598177a56
Marcelo Puebla <marcenp23@gmail.com>
2024-01-04 17:46:57 -0300

    feat(note-view): show data in note view section

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,9 +1,19 @@
 import { SaveOutlined } from '@mui/icons-material';
 import { Button, Grid, TextField, Typography } from '@mui/material';
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
 import { ImageGallery } from '../components';
 
 export const NoteView = () => {
+  const { active: note } = useSelector((state) => state.journal);
+  const { body, date, title, onInputChange, formState } = useForm(note);
+
+  const dateString = useMemo(() => {
+    const newDate = new Date(date);
+    return newDate.toUTCString();
+  }, [date]);
+
   return (
     <Grid
       className="animate__animated animate__fadeIn animate__faster"
@@ -14,14 +24,14 @@ export const NoteView = () => {
       sx={{ mb: 1 }}>
       <Grid item>
         <Typography fontSize={39} fontWeight="light">
-          28 Feb, 2022
+          {dateString}
         </Typography>
       </Grid>
 
       <Grid item>
         <Button color="primary" sx={{ padding: 1 }}>
           <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
-          Guardar
+          Save
         </Button>
       </Grid>
 
@@ -33,8 +43,21 @@ export const NoteView = () => {
           placeholder="Add a title"
           label="Title"
           sx={{ border: 'none', mb: 1 }}
+          name="title"
+          value={title}
+          onChange={onInputChange}
         />
-        <TextField type="text" variant="filled" fullWidth multiline placeholder="What happened today?" minRows={5} />
+        <TextField
+          type="text"
+          variant="filled"
+          fullWidth
+          multiline
+          placeholder="What happened today?"
+          minRows={5}
+          name="body"
+          value={body}
+          onChange={onInputChange}
+        />
       </Grid>
 
       {/* Image gallery */}
