9e1462aa68b78d57ba7dc4f524a02cfe8923c0e9
Marcelo Puebla <marcenp23@gmail.com>
2024-01-04 17:29:43 -0300

    feat(sidebar): set active notes when click

--- src/journal/components/SidebarItem.jsx
+++ src/journal/components/SidebarItem.jsx
@@ -1,15 +1,23 @@
 import { TurnedInNot } from '@mui/icons-material';
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
 import React, { useMemo } from 'react';
+import { useDispatch } from 'react-redux';
+import { setActiveNote } from '../../store/journal/journalSlice';
 
-export const SidebarItem = ({ body, id, title }) => {
+export const SidebarItem = ({ body, date, id, imageUrls = [], title }) => {
+  const dispatch = useDispatch();
+
   const newTitle = useMemo(() => {
     return title.length > 16 ? title.substring(0, 16) + '...' : title;
   }, [title]);
 
+  const onClickNote = () => {
+    dispatch(setActiveNote({ body, id, imageUrls, title, date }));
+  };
+
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onClickNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
